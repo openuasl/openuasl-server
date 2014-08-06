692cb36407d4bc7b8c083df0a692c77f68d7c739
JihyeonKwon <wlgus921211@naver.com>
2014-08-06 23:59:43 +0900

    sdfsdfc

--- openuasl-server/src/main.cpp
+++ openuasl-server/src/main.cpp
@@ -3,14 +3,23 @@
 
 #include <openuasl/ucstream/UCStreamServer.h>
 #include <openuasl/main_server.h>
+#include <boost/thread.hpp>
+
+void uavcam_server_thread(){
+	try{
+	
+		openuasl::server::ucstream::UCStreamServer s(UCS_SERVER_PORT);
+		s.Run();
+	}catch(std::exception& e){
+		std::cout << e.what() << std::endl;
+	}
+}
 
 
 int main(void){
-	
-	try{
-		openuasl::server::ucstream::UCStreamServer s(UCS_SERVER_PORT);
-		s.Run();
+	boost::thread uc_thread(&uavcam_server_thread);
 
+	try{
 		openuasl::server::main_server main_s(SERVER_PORT);
 		main_s.Run();
 
